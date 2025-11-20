724df78b263c302b80bd74c85cd0efa06fb25d46
hayato0223-lgtm <hayatosaiki0223@gmail.com>
2025-11-20 16:24:42 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -7,4 +7,4 @@ st.write(name)
 
 camera = st.camera_input("写真を撮影しますわ")
 if camera:
-  　st.image(camera, caption="写真", use_column_width=True)
+      st.image(camera, caption="写真", use_column_width=True)
