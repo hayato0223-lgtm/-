1d1dda25c83d918f564f1b7466e22037d88e1a48
hayato0223-lgtm <hayatosaiki0223@gmail.com>
2025-11-20 16:22:30 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title("こんにちは吉村ゼミ")
+st.text_input("好きな言葉を入力してください")
+
+st.write(name)
+
+camera = st.camera_input("写真を撮影しますわ")
+if camera:
+  　st.image(camera, caption="写真", use_column_width=True)
