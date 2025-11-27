843c81e34ff36a40b09b96d84e2c9f25794a7622
hayato0223-lgtm <hayatosaiki0223@gmail.com>
2025-11-27 15:25:58 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -6,7 +6,7 @@ name = st.text_input("好きな言葉を入力してください")
 st.write(name)
 
 st.checkbox("同意します")
-st.selectbox("次の中から現住所を教えてください,["京都","大阪"])
+st.selectbox("次の中から現住所を教えてください”,["京都","大阪"])
 camera = st.camera_input("写真を撮影しますわ")
 if camera:
       st.image(camera, caption="写真", use_column_width=True)
