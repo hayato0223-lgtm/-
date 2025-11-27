b85fc51282e42957f995cd0ef1d747c2709b48b1
hayato0223-lgtm <hayatosaiki0223@gmail.com>
2025-11-27 15:45:25 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -13,6 +13,11 @@ st.write(hobby)
 scotre = st.slider("この映画を10点満点で評価してください",0,10,0)
 choice = st.radio("性別を選択してください",["男性","女性"])
 st.write(choice)
+list = [
+      {"latitude":35.05,"longitude":135.76},
+      {"latitude":35.04,"longitude":135.75},
+]
+st.map(list)
 camera = st.camera_input("写真を撮影しますわ")
 if camera:
       st.image(camera, caption="写真", use_column_width=True)
