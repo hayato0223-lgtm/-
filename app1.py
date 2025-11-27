58de5b74d6cbc08b3511e33980ac8aae202265ba
hayato0223-lgtm <hayatosaiki0223@gmail.com>
2025-11-27 15:32:42 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -9,6 +9,7 @@ st.checkbox("同意します")
 address = st.selectbox("次の中から現住所を教えてください",["京都","大阪"])
 st.write(address)
 hobby = st.multiselect("趣味を次から複数選択してください",["映画","漫画"])
+st.write(hobby)
 camera = st.camera_input("写真を撮影しますわ")
 if camera:
       st.image(camera, caption="写真", use_column_width=True)
