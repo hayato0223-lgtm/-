9d01948e7baa68878e5d27a6a47aa630b471adfe
hayato0223-lgtm <hayatosaiki0223@gmail.com>
2025-11-27 15:21:12 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,6 +5,7 @@ name = st.text_input("好きな言葉を入力してください")
 
 st.write(name)
 
+st.checkbox("同意します")
 camera = st.camera_input("写真を撮影しますわ")
 if camera:
       st.image(camera, caption="写真", use_column_width=True)
